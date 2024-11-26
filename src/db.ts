58bf4dc51f89e8d1cd2bc5aafe312e4535542875
Piyushzingade <piyushzingade@gmail.com>
2024-11-26 13:07:17 +0530

    Completed

--- src/db.ts
+++ src/db.ts
@@ -1,10 +1,27 @@
-import mongoose from "mongoose";
+import mongoose, { model, Schema } from "mongoose";
 
-export const connectToDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGODB_URL!);
-    console.log("Connected to MongoDB");
-  } catch (err) {
-    console.log(err);
-  }
-};
+mongoose.connect("mongodb://localhost:27017/brainly");
+
+const UserSchema = new Schema({
+  username: { type: String, unique: true },
+  password: String,
+});
+
+export const UserModel = model("User", UserSchema);
+
+const ContentSchema = new Schema({
+  title: String,
+  link: String,
+  tags: [{ type: mongoose.Types.ObjectId, ref: "Tag" }],
+  userId: { type: mongoose.Types.ObjectId, ref: "User", required: true },
+});
+
+export const ContentModel = model("Content", ContentSchema);
+
+
+const LinkSchema = new Schema({
+  hash: String,
+  userId: { type: mongoose.Types.ObjectId, ref: "User", required: true , unique: true },
+});
+
+export const LinkModel = model("Link", LinkSchema);
