0d1d1dbfb684e903681cb9f7b7c3e4cbf425b933
Piyushzingade <piyushzingade@gmail.com>
2024-12-02 11:58:15 +0530

    Updated middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,7 +7,7 @@ export const userMiddleware = (
   res: Response,
   next: NextFunction
 ) => {
-  const header = req.headers["authorization"];
+  const header = req.headers.token;
   const decoded = jwt.verify(header as string, JWT_PASSWORD);
   if (decoded) {
     if (typeof decoded === "string") {
