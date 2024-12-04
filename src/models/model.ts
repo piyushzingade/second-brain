7028c2a4027053ab66674276eba3256fcbeacfbc
Piyushzingade <piyushzingade@gmail.com>
2024-12-04 21:44:56 +0530

    Updated model

---
+++ src/models/model.ts
@@ -0,0 +1,31 @@
+import mongoose, { model  , Schema} from "mongoose";
+
+
+const UserSchema = new Schema({
+  username: { type: String, unique: true },
+  password: String,
+});
+
+export const UserModel = model("User", UserSchema);
+
+const ContentSchema = new Schema({
+  title: String,
+  link: String,
+  type: String,
+  tags: [{ type: mongoose.Types.ObjectId, ref: "Tag" }],
+  userId: { type: mongoose.Types.ObjectId, ref: "User", required: true },
+});
+
+export const ContentModel = model("Content", ContentSchema);
+
+const LinkSchema = new Schema({
+  hash: String,
+  userId: {
+    type: mongoose.Types.ObjectId,
+    ref: "User",
+    required: true,
+    unique: true,
+  },
+});
+
+export const LinkModel = model("Link", LinkSchema);
