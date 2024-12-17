0f00a4584383abc790b4aaeacd6013361851fcf5
Piyushzingade <piyushzingade@gmail.com>
2024-12-17 13:58:58 +0530

    Updated

--- src/controllers/Auth.ts
+++ src/controllers/Auth.ts
@@ -12,11 +12,11 @@ dotenv.config();
 const reqbody=z.object({
    email:z.string(),
    password:z.string().min(6, { message: "Password must be at least 8 characters long." })
-   .max(32, { message: "Password must not exceed 32 characters." })
-   .regex(/[A-Z]/, { message: "Password must contain at least one uppercase letter." })
-   .regex(/[a-z]/, { message: "Password must contain at least one lowercase letter." })
-   .regex(/[0-9]/, { message: "Password must contain at least one number." })
-   .regex(/[@$!%*?&]/, { message: "Password must contain at least one special character." })
+   // .max(32, { message: "Password must not exceed 32 characters." })
+   // .regex(/[A-Z]/, { message: "Password must contain at least one uppercase letter." })
+   // .regex(/[a-z]/, { message: "Password must contain at least one lowercase letter." })
+   // .regex(/[0-9]/, { message: "Password must contain at least one number." })
+   // .regex(/[@$!%*?&]/, { message: "Password must contain at least one special character." })
  
 })
    
